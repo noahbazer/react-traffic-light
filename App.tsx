2bb9ac49c99bf0d6adda954288e31bbb55f97b4f
Noah Bazer <nbazer00@gmail.com>
2024-01-29 22:30:05 -0600

    create root, set render

--- App.tsx
+++ App.tsx
@@ -1,1 +1,16 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { TrafficLightContainer } from './components/TrafficLightContainer';
+
+export function App() {
+  return (
+    <>
+        <TrafficLightContainer />
+    </>
+  );
+}
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(<App />);
